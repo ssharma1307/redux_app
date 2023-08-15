87da0691630a43bd666c429edc6e6096738eee9d
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-08-15 18:47:48 -0400

    action and reducer component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
-    
+    <Navbar/>
+    <div className="container">
+    <Shop/>
+    </div>
     </>
     
   );
